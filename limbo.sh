70bfbebdf0bc7fd9956c8a97a49f67d3daa3762d
Hadrien Mens-Pellen <hmens@financeactive.com>
2019-05-15 16:50:44 +0200

    Attempt to improve conflict management

--- limbo.sh
+++ limbo.sh
@@ -24,7 +24,7 @@ while(true);
 	BASE=$(git merge-base @ "$UPSTREAM")
 
 	if [ $LOCAL = $REMOTE ]; then
-		if [ $conflictsToSolve ]; then
+		if [ $conflictsToSolve -eq 1 ]; then
 			conflictsToSolve=0
 			endspin;
 		fi
