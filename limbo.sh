1082be739a36ed57ea4c81cf57e59c6c72e7765a
Hadrien Mens-Pellen <hmens@financeactive.com>
2019-05-15 13:06:44 +0200

    Push only when needed

--- limbo.sh
+++ limbo.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
 while(true);
- do
- echo "";
- echo "----";
- echo "Pull";
- echo "----";
- git pull --rebase;
- echo "----";
- echo "Push";
- echo "----";
- git push;
- done;
+  do
+  echo "";
+  echo "----";
+  echo "Pull";
+  echo "----";
+  git pull --rebase;
+
+  if [[ $(git cherry) ]]; then
+    echo "----";
+    echo "Push";
+    echo "----";
+    git push;
+  fi
+  done;
