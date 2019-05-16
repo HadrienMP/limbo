5d518927342c44a9ad02059c506096c62330b454
Hadrien Mens-Pellen <hmens@financeactive.com>
2019-05-16 09:17:12 +0200

    Attempt to refactor

--- limbo.sh
+++ limbo.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # ------------
-# Pull Spinner
+# Spinner
 # ------------
 sp='/-\|'
 sc=0
 spin() {
-    echo -en "\rWaiting for change ${sp:sc++:1}"
+    echo -en "\r$1 ${sp:sc++:1}"
     ((sc==${#sp})) && sc=0
 }
 endspin() {
@@ -16,6 +16,76 @@ endspin() {
 waitingForChange=0
 conflictsToSolve=0
 
+# ------------
+# Wait
+# ------------
+wait() {
+    if [ $conflictsToSolve -eq 1 ]; then
+        conflictsToSolve=0
+    fi
+    spin "Waiting for change"
+    waitingForChange=1
+}
+
+# ------------
+# PULL
+# ------------
+pull() {
+    if [ $conflictsToSolve -eq 1 ]; then
+        conflictsToSolve=0
+    fi
+    if [ $waitingForChange -eq 1 ]; then
+        waitingForChange=0
+        endspin
+    fi
+    echo "-----------------------------";
+    echo "Pull";
+    echo "-----------------------------";
+    git pull --rebase;
+    echo "-----------------------------";
+    echo -e "\n"
+}
+# ------------
+# PUSH
+# ------------
+push() {
+    if [ $conflictsToSolve -eq 1 ]; then
+        conflictsToSolve=0
+    fi
+    if [ $waitingForChange -eq 1 ]; then
+        waitingForChange=0
+        endspin;
+    fi
+    echo "-----------------------------";
+    echo "Push";
+    echo "-----------------------------";
+    git push;
+    echo "-----------------------------";
+    echo -e "\n"
+}
+
+# ------------
+# Conflicts
+# ------------
+handleConflicts() {
+    if [[ $waitingForChange -eq 1 ]]; then
+        waitingForChange=0
+        endspin
+    fi
+    if [[ $conflictsToSolve -eq 0 ]]; then
+        echo -e "\033[0;31mBEWARE ! No action possible, fix the divergence (possible rebase conflicts).\033[0m"
+        read -p "Would like to reset your local copy to match the remote one ? (y/N) " reset
+        if [[ reset -eq "y" ]]; then
+            remoteBranch=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/@@')
+            git reset --hard $remoteBranch
+        fi
+        conflictsToSolve=1
+    fi
+}
+
+# ------------
+# LOOP
+# ------------
 while(true);
   do
   	git remote update &> /dev/null;
@@ -25,53 +95,13 @@ while(true);
 	BASE=$(git merge-base @ "$UPSTREAM")
 
 	if [ $LOCAL = $REMOTE ]; then
-		if [ $conflictsToSolve -eq 1 ]; then
-			conflictsToSolve=0
-		fi
-	    spin
-	    waitingForChange=1
+        wait;
 	elif [ $LOCAL = $BASE ]; then
-		if [ $conflictsToSolve -eq 1 ]; then
-			conflictsToSolve=0
-		fi
-		if [ $waitingForChange -eq 1 ]; then
-			waitingForChange=0
-			endspin
-		fi
-		echo "-----------------------------";
-		echo "Pull";
-		echo "-----------------------------";
-		git pull --rebase;
-		echo "-----------------------------";
-		echo -e "\n"
+        pull;
 	elif [ $REMOTE = $BASE ]; then
-		if [ $conflictsToSolve -eq 1 ]; then
-			conflictsToSolve=0
-		fi
-		if [ $waitingForChange -eq 1 ]; then
-			waitingForChange=0
-    		endspin;
-		fi
-		echo "-----------------------------";
-		echo "Push";
-		echo "-----------------------------";
-		git push;
-		echo "-----------------------------";
-		echo -e "\n"
+        push;
 	else
-		if [ $waitingForChange -eq 1 ]; then
-			waitingForChange=0
-			endspin
-		fi
-		if [ $conflictsToSolve -eq 0 ]; then
-		    echo -e "\033[0;31mBEWARE ! No action possible, fix the divergence (possible rebase conflicts).\033[0m"
-		    read -p "Would like to reset your local copy to match the remote one ? (y/N) " reset
-		    if [[ reset -eq "y" ]]; then
-				remoteBranch=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/@@')
-				git reset --hard $remoteBranch
-		    fi
-			conflictsToSolve=1
-		fi
+		handleConflicts;
 	fi
   done;
 
